5596a0293fb0fb04382b49114742d39300c23d76
Makenna Smutz <makennasmutz@gmail.com>
2021-07-19 00:05:35 +0200

    add more data to the query

--- src/utils/graphql-operations.ts
+++ src/utils/graphql-operations.ts
@@ -30,7 +30,24 @@ export const GET_PROPERTIES = gql`
 export const GET_HOUSE = gql`
   query GET_HOUSE($houseID: ID!) {
     findHouseByID(id: $houseID) {
+      _id
       title
+      naslov
+      description
+      opis
+      images
+      type
+      transaction
+      bedRooms
+      bathRooms
+      floorSize
+      plotSize
+      totalPrice
+      location {
+        neighborhood
+        latitude
+        longitude
+      }
     }
   }
 `;
