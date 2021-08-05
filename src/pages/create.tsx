24d652995bf669d43d528b85e84c9846b17a1f29
Makenna Smutz <makennasmutz@gmail.com>
2021-08-05 21:40:30 +0200

    File upload to s3 (#4)
    * bare bones of s3 upload
    
    * multiple images
    
    * add keys to env template
    
    * silence type errors
    
    * rename import

--- src/pages/create.tsx
+++ src/pages/create.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { CreatePropertyForm } from '../components/create-form';
+import { UploadSingleImage } from '../components/image-input';
 import { Layout } from '../components/layout';
 
 const CreatePage = () => {
   return (
     <Layout>
       <CreatePropertyForm />
+      <UploadSingleImage />
     </Layout>
   );
 };
