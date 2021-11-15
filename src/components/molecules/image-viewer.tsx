88e2e92efdea2c0f1a419244e52b0d543eb38df9
Makenna Smutz <makennasmutz@gmail.com>
2021-11-15 12:11:53 +0100

    image viewer

--- src/components/molecules/image-viewer.tsx
+++ src/components/molecules/image-viewer.tsx
@@ -1,19 +1,26 @@
-import React from 'react';
-import { Box, Wrap, WrapItem } from '@chakra-ui/react';
+import React, { useState } from 'react';
+import { Box, Image, Wrap, WrapItem } from '@chakra-ui/react';
 
 type ImageViewerProps = {
   images: [string];
 };
 
 export const ImageViewer = ({ images }: ImageViewerProps) => {
+  const [imageSelected, setImageSelected] = useState(images[0]);
+
   return (
     <Box mr={8}>
-      <img src={images[0]} style={{ borderRadius: 8, marginBottom: 8 }} />
+      <img src={imageSelected} style={{ borderRadius: 8, marginBottom: 8 }} />
       <Wrap>
         {images.map((image: string, i: number) => (
           <WrapItem>
-            <img
-              style={{ maxWidth: '100px', borderRadius: 6 }}
+            <Image
+              onClick={() => setImageSelected(image)}
+              maxW="100px"
+              borderRadius="md"
+              outline={imageSelected === image ? `2px solid` : 'none'}
+              outlineColor={imageSelected === image ? 'emerald.500' : 'none'}
+              outlineOffset="1px"
               src={image}
               key={i}
             />
