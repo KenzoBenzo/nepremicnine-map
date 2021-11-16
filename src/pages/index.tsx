1825784dcb709194c6837afbb66e57003c2991fe
Makenna Smutz <makennasmutz@gmail.com>
2021-11-16 13:11:00 +0100

    Add search bar stubs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,61 @@
 import React from 'react';
-import { Box, Flex, Heading } from '@chakra-ui/react';
+import {
+  Box,
+  Center,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Stack,
+  useColorModeValue,
+} from '@chakra-ui/react';
 import Image from 'next/image';
 import house from '../../public/House.png';
+import SegmentedControl from '../components/atoms/segmented-control';
 
 const Index = () => {
+  const searchInputBackground = useColorModeValue('white', 'gray.900');
   return (
-    <Box
-      mt={20}
-      bgGradient="linear(to-br, blue.50, blue.200)"
-      w="100%"
-      maxW="1000px"
-      minH="350px"
-      borderRadius="lg"
-      overflow="hidden"
-    >
-      <Heading m={8}>Easy way to find the perfect property</Heading>
-      <Flex w="full" justify="flex-end">
-        <Image src={house} alt="Picture of a house" />
-      </Flex>
-    </Box>
+    <>
+      <Box
+        mt={20}
+        bgGradient="linear(to-br, blue.50, blue.200)"
+        background="url('https://media.graphcms.com/hofevqGT6CWIlQIogRAV'), linear-gradient(to bottom right, blue.50, blue.200)"
+        w="100%"
+        maxW="1000px"
+        minH="350px"
+        borderRadius="lg"
+        overflow="hidden"
+      >
+        <Heading m={8}>Easy way to find the perfect property</Heading>
+      </Box>
+
+      <Box mt={-28} mx="auto">
+        <SegmentedControl items={['Rent', 'Buy']} />
+        <Stack
+          direction="row"
+          backgroundColor={searchInputBackground}
+          borderRadius="lg"
+          maxW="800px"
+          p={6}
+          boxShadow="base"
+        >
+          <FormControl>
+            <FormLabel htmlFor="location">Location</FormLabel>
+            <Input type="text" id="location" />
+          </FormControl>
+          <FormControl>
+            <FormLabel htmlFor="location">Location</FormLabel>
+            <Input type="text" id="location" />
+          </FormControl>
+          <FormControl>
+            <FormLabel htmlFor="location">Location</FormLabel>
+            <Input type="text" id="location" />
+          </FormControl>
+        </Stack>
+      </Box>
+    </>
   );
 };
 
