00773fbce0882a724734e9645614f32cb78acec6
Makenna Smutz <makennasmutz@gmail.com>
2021-11-16 11:32:03 +0100

    Change homepage, update logo and add hero

---
+++ src/pages/houses/index.tsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import dynamic from 'next/dynamic';
+import { Box, Stack, Text, Center, Spinner } from '@chakra-ui/react';
+import Card from '../../components/molecules/listing-card';
+import ListingGrid from '../../components/organisms/listing-grid';
+import { fetcher } from '../../utils/graphql-client';
+import useSWR from 'swr';
+import { GET_PROPERTIES } from '../../utils/graphql-operations';
+import { Property } from '../../utils/types';
+import { MarkerAndPopup } from '../../components/atoms/popup-and-marker';
+
+const DynamicMapWithNoSSR = dynamic(
+  () => import('../../components/organisms/map'),
+  {
+    ssr: false,
+  }
+);
+
+const Index = () => {
+  const {
+    data: fetchedData,
+    error,
+    isValidating,
+  } = useSWR(GET_PROPERTIES, fetcher);
+
+  if (error) {
+    return <Text color="red.500">{JSON.stringify(error, null, 2)}</Text>;
+  }
+
+  if (isValidating && !fetchedData) {
+    return (
+      <Center h="90vh">
+        <Spinner /> <Text ml={3}>Loading</Text>
+      </Center>
+    );
+  }
+
+  return (
+    <Stack
+      direction={['column-reverse', 'row']}
+      spacing={0}
+      h="100vh"
+      w="100%"
+      overflow="auto"
+    >
+      <Box w="100%" mx={8}>
+        <ListingGrid>
+          {fetchedData?.houses?.data?.map((house: Property, index: number) => (
+            <Card
+              key={index}
+              image={house.images[0]}
+              title={house.title}
+              bed={house.bedRooms || 0}
+              bath={house.bathRooms || 0}
+              floor={house.floorSize || 0}
+              plot={house.plotSize || 0}
+              pricing={house.totalPrice}
+              location={house.location.neighborhood}
+              id={house._id}
+            />
+          ))}
+        </ListingGrid>
+      </Box>
+
+      <Box h="100%" w="100%" maxW={600}>
+        <DynamicMapWithNoSSR>
+          <>
+            {fetchedData?.houses?.data?.map(
+              (property: Property, index: number) => (
+                <MarkerAndPopup
+                  key={index}
+                  latitude={property.location.latitude}
+                  longitude={property.location.longitude}
+                  image={property.images[0]}
+                  title={property.title}
+                  price={property.totalPrice}
+                />
+              )
+            )}
+          </>
+        </DynamicMapWithNoSSR>
+      </Box>
+    </Stack>
+  );
+};
+
+export default Index;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,86 +1,24 @@
 import React from 'react';
-import dynamic from 'next/dynamic';
-import { Box, Stack, Text, Center, Spinner } from '@chakra-ui/react';
-import Card from '../components/molecules/listing-card';
-import ListingGrid from '../components/organisms/listing-grid';
-import { fetcher } from '../utils/graphql-client';
-import useSWR from 'swr';
-import { GET_PROPERTIES } from '../utils/graphql-operations';
-import { Property } from '../utils/types';
-import { MarkerAndPopup } from '../components/atoms/popup-and-marker';
-
-const DynamicMapWithNoSSR = dynamic(
-  () => import('../components/organisms/map'),
-  {
-    ssr: false,
-  }
-);
+import { Box, Flex, Heading } from '@chakra-ui/react';
+import Image from 'next/image';
+import house from '../../public/House.png';
 
 const Index = () => {
-  const {
-    data: fetchedData,
-    error,
-    isValidating,
-  } = useSWR(GET_PROPERTIES, fetcher);
-
-  if (error) {
-    return <Text color="red.500">{JSON.stringify(error, null, 2)}</Text>;
-  }
-
-  if (isValidating && !fetchedData) {
-    return (
-      <Center h="90vh">
-        <Spinner /> <Text ml={3}>Loading</Text>
-      </Center>
-    );
-  }
-
   return (
-    <Stack
-      direction={['column-reverse', 'row']}
-      spacing={0}
-      h="100vh"
+    <Box
+      mt={20}
+      bgGradient="linear(to-br, blue.50, blue.200)"
       w="100%"
-      overflow="auto"
+      maxW="1000px"
+      minH="350px"
+      borderRadius="lg"
+      overflow="hidden"
     >
-      <Box w="100%" mx={8}>
-        <ListingGrid>
-          {fetchedData?.houses?.data?.map((house: Property, index: number) => (
-            <Card
-              key={index}
-              image={house.images[0]}
-              title={house.title}
-              bed={house.bedRooms || 0}
-              bath={house.bathRooms || 0}
-              floor={house.floorSize || 0}
-              plot={house.plotSize || 0}
-              pricing={house.totalPrice}
-              location={house.location.neighborhood}
-              id={house._id}
-            />
-          ))}
-        </ListingGrid>
-      </Box>
-
-      <Box h="100%" w="100%" maxW={600}>
-        <DynamicMapWithNoSSR>
-          <>
-            {fetchedData?.houses?.data?.map(
-              (property: Property, index: number) => (
-                <MarkerAndPopup
-                  key={index}
-                  latitude={property.location.latitude}
-                  longitude={property.location.longitude}
-                  image={property.images[0]}
-                  title={property.title}
-                  price={property.totalPrice}
-                />
-              )
-            )}
-          </>
-        </DynamicMapWithNoSSR>
-      </Box>
-    </Stack>
+      <Heading m={8}>Easy way to find the perfect property</Heading>
+      <Flex w="full" justify="flex-end">
+        <Image src={house} alt="Picture of a house" />
+      </Flex>
+    </Box>
   );
 };
 
